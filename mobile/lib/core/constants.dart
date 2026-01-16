008eb06e2b9354ba07af74a047297b4477e3ad85
Coder69-ops <20232002010@nwu.ac.bd>
2026-01-16 12:07:51 +0600

    feat: ff

--- mobile/lib/core/constants.dart
+++ mobile/lib/core/constants.dart
@@ -1,6 +1,6 @@
 
 class Constants {
-  static const String baseUrl = 'http://13.63.34.104:3000';
+  static const String baseUrl = 'https://api.nwuconnect.aixplore.me';
 
   static const List<String> departments = [
     'CSE',
